c46968cbc0fb56a81f7429b44cabe0a0e16c1a0b
Samuel Yvon <samuelyvon9@gmail.com>
2023-03-13 13:38:15 -0400

    Fix upstream questionning

--- gscp/git.py
+++ gscp/git.py
@@ -5,6 +5,7 @@ from .wrappers import stdout_of_proc
 
 def git_current_branch() -> str:
     command = ["git", "branch", "--show-current"]
-    return stdout_of_proc(
+    out = stdout_of_proc(
         subprocess.run(command, capture_output=True, check=True, timeout=10)
     )
+    return out.strip()
